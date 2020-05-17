0f63b031cdb5b387cc020e200723d00527f51704
Carlos Revilla <clro26.k@gmail.com>
2020-05-18 00:16:32 +0200

    hover effect and style to the components

--- frontend/src/containers/Home/Home.jsx
+++ frontend/src/containers/Home/Home.jsx
@@ -22,7 +22,9 @@ const Home = (props) => {
                 <p>{character.name}</p>
                 <img src={character.image} alt="" />
             </div>)}
-            <Pagination defaultCurrent={1} defaultPageSize={20} total={totalPages} onChange={onPageChange} showSizeChanger={false} />
+            <div className="box-btn">
+                <Pagination defaultCurrent={1} defaultPageSize={20} total={totalPages} onChange={onPageChange} showSizeChanger={false} />
+            </div>
         </div>
     )
 }
